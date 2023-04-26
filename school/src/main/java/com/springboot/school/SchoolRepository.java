02dbc16b7423391d74fb9665c71c89c20d39c618
Abderrahmen dai <abderrahmen.dai.11@gmail.com>
2023-04-27 00:42:09 +0100

    School microservice

---
+++ school/src/main/java/com/springboot/school/SchoolRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.school;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SchoolRepository extends JpaRepository<School, Integer> {
+
+    List<School> findAllBySchoolId(Integer schoolId);
+
+}
